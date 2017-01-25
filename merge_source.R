96235c419cb97c2761ebae23444d7c1cdccce0b0
Elliot Francis <signup@windowlatch.org>
2017-01-25 13:46:03 -0700

    Dataset Merge

---
+++ merge_source.R
@@ -0,0 +1,16 @@
+source("get_activity_key.R")
+source("get_features_key.R")
+source("get_dataset.R")
+merge_source <- function (limit=NULL) {
+  activity_key <- get_activity_key()
+  feature_key <- get_features_key()
+  if(is.null(limit)) {
+    test_dataset <- get_dataset(file.path("Dataset","test"), activity_key, feature_key)
+    train_dataset <- get_dataset(file.path("Dataset","train"), activity_key, feature_key)  
+  } else {
+    test_dataset <- get_dataset(file.path("Dataset","test"), activity_key, feature_key, 5)
+    train_dataset <- get_dataset(file.path("Dataset","train"), activity_key, feature_key, 5)
+  }
+  merged_data = rbind(test_dataset, train_dataset)
+  return(merged_data)
+}
