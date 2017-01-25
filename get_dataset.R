71de235536cefffab6fe34b3c97875f0c7d0c3b9
Elliot Francis <signup@windowlatch.org>
2017-01-25 14:19:14 -0700

    Added Subject ID

--- get_dataset.R
+++ get_dataset.R
@@ -4,20 +4,24 @@ get_dataset <- function (path, activity_key, feature_key, limit=NULL) {
   files <- dir(path);
   data_filename <- files[grep("^X_", files)]
   label_filename <- files[grep("^y_", files)]
+  subject_filename <- files[grep("^subject_", files)]
   
   #Read Source Data
   if(is.null(limit)) {
     source_data <- read.fwf(file.path(path, data_filename), rep(16, nrow(feature_key)))  
     source_label <- read.fwf(file.path(path, label_filename), c(1))
+    source_subject <- read.csv(file.path(path, subject_filename), header=FALSE)
   } else {
     source_data <- read.fwf(file.path(path, data_filename), rep(16, nrow(feature_key)), n=limit)
     source_label <- read.fwf(file.path(path, label_filename), c(1), n=limit)
+    source_subject <- read.csv(file.path(path, subject_filename), header=FALSE, nrows=limit)
   }
   
   data_key <- feature_key$feature_label;
-
+  
   colnames(source_data) <- data_key;
   colnames(source_label) <- c("activity_id");
+  colnames(source_subject) <- c("subject_id");
   source_label <- merge(source_label, activity_key, by.x="activity_id", by.y="activity_id")
   if(nrow(source_data) != nrow(source_label)) { 
     print(nrow(source_data))
@@ -26,6 +30,7 @@ get_dataset <- function (path, activity_key, feature_key, limit=NULL) {
   }
   
   merged <- cbind(source_label, source_data);
+  merged <- cbind(source_subject, merged);
   
   return(merged)
   
